53a257eb2744e23ec4e4cf75f34bd7bb1c115a9a
matrayu <northridge614@gmail.com>
2019-09-15 12:05:05 -0700

    Auth router endpoint tests all working

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/tickets', ticketsRouter)
 app.use('/api/users', usersRouter)
-app.use('api/auth', authRouter)
+app.use('/api/auth', authRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
--- test/setup.js
+++ test/setup.js
@@ -2,5 +2,12 @@ require('dotenv').config()
 const { expect } = require('chai')
 const supertest = require('supertest')
 
+process.env.TZ = 'UTC'
+process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'change-this-secret'
+process.env.JWT_EXPIRY = '3h'
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://matrayu@localhost/numerology_test"
+
 global.expect = expect
 global.supertest = supertest
