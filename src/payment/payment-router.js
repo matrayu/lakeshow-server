561ef9e545d191d3991b0079048ebf1e47a4dc6c
matrayu <northridge614@gmail.com>
2019-11-10 11:28:39 -0800

    Update payment-router.js

--- src/payment/payment-router.js
+++ src/payment/payment-router.js
@@ -1,22 +1,8 @@
 const express = require('express')
-/* const paypal = require('paypal-rest-sdk') */
-/* const { PAYPAL_CID, PAYPAL_SECRET } = require('../config') */
 const PaymentService = require('./payment-service')
-const EmailRouter = require('../email/email-router')
-
 const { requireAuth } = require('../middleware/jwt-auth')
-
 const paymentRouter = express.Router()
 const jsonBodyParser = express.json()
-
-// configure paypal with the credentials you got when you created your paypal app
-/* paypal.configure({
-    'mode': 'sandbox', //sandbox or live 
-    'client_id': PAYPAL_CID, // please provide your client id here 
-    'client_secret': PAYPAL_SECRET // provide your client secret here 
-}); */
-  
-  
 
 // start payment process
 paymentRouter
