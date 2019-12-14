53476b81c6fb5a1d7db5d69eb6d9cecd674f8b67
matrayu <northridge614@gmail.com>
2019-12-14 15:19:07 -0800

    Update 001.undo.create_db_full.sql

--- migrations/001.undo.create_db_full.sql
+++ migrations/001.undo.create_db_full.sql
@@ -10,33 +10,9 @@ DROP TABLE IF EXISTS "ref_invoice_status_codes";
 DROP TABLE IF EXISTS "ref_payment_methods";
 DROP TABLE IF EXISTS "seller_products";
 DROP TABLE IF EXISTS "product_images";
+DROP TABLE IF EXISTS "product_listings_users";
 DROP TABLE IF EXISTS "products";
 DROP TABLE IF EXISTS "games";
 DROP TABLE IF EXISTS "venues";
 DROP TABLE IF EXISTS "teams";
 DROP TABLE IF EXISTS "users";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
