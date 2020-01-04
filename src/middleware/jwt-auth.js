fabe81d4ab7edaa0b73be9024ec9aa1c1fb9db15
matrayu <northridge614@gmail.com>
2020-01-03 18:45:15 -0800

    Update jwt-auth.js

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,6 +1,7 @@
 const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
+
     const authToken = req.get('Authorization') || ''
 
     let bearerToken
@@ -14,16 +15,12 @@ function requireAuth(req, res, next) {
     try {
         const payload = AuthService.verifyJwt(bearerToken)
         
-        AuthService.getUserWithUserName(
-            req.app.get('db'),
-            payload.sub,
-        )
+        AuthService.getUserWithUserName(req.app.get('db'),payload.sub)
             .then(user => {
                 if (!user) {
                     return res.status(401).json({ error: 'Unauthorized request' })
                 }
-                req.user = user
-                next()
+                return
             })
             .catch(err => {
                 console.error(err)
